23965eba928682784023883ea22086a6d672e8ee
almaktoom <156554238+almaktoom@users.noreply.github.com>
2024-02-20 19:02:13 +0000

    first commit
    first commit

---
+++ src/main/java/spring/net/iibrary/IibraryApplication.java
@@ -0,0 +1,13 @@
+package spring.net.iibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/net/iibrary/IibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.net.iibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
